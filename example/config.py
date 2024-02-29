c5a6fd88e1cf73d22747e725c43a861bd5f1a0fa
Koen Keijzer <koen19@gmail.com>
2024-02-29 13:40:25 +0100

    Made example work

--- example/config.py
+++ example/config.py
@@ -9,7 +9,13 @@ sg.add_template('sh', "template.sh")
 sg.add_template('slurm', "job.slurm")
 
 sg.add_replacement_rule('sh', rr_Alice_shell())
-sg.add_global_rule('slurm', gr_Alice_slurm())
+
+
+gr = gr_Alice_slurm()
+gr.set_time("0-03:00:00")
+gr.set_node("cpu-short")
+
+sg.add_global_rule('slurm', gr)
 
 import itertools
 sg.parameters = list(itertools.product(Parameters("lambda", [1, 2, 4]).values, Parameters("it", list(range(3))).values))
